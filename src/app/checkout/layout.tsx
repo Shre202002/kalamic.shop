764914ae7eac7ffaac8b73160a8a0cd3f429dffb
Sriyansh Gupta <sriyanshgupta24@gmail.com>
2026-04-23 15:48:59 +0000

    I need to make the following changes to my Next.js 15 project. Please m

---
+++ src/app/checkout/layout.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  robots: { index: false, follow: false }
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
